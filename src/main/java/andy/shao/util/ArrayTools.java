caf9ae370ef44de1277d734492d1a4c3058483f0
Andy-Shao <hj13035952619@126.com>
2014-03-04 00:31:13 +0800

    ss

--- src/main/java/andy/shao/util/ArrayTools.java
+++ src/main/java/andy/shao/util/ArrayTools.java
@@ -1,3 +1,24 @@
 package andy.shao.util;
 
-public final class ArrayTools{}
+import java.lang.reflect.Array;
+
+/**
+ * 
+ * Title: Some tools of Array<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) Mar 4, 2014<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+public final class ArrayTools{
+    
+    @SuppressWarnings("unchecked")
+    public static <T> T[] mergeArrays(Class<T[]> array_type, T[]...arrays){
+        int length = 0;
+        for(T[] array : arrays) length += array.length;
+        T[] result = (T[]) Array.newInstance(array_type.getComponentType() , length);
+        for(int i=0, index=0; i<arrays.length; index+=arrays[i].length, i++) System.arraycopy(arrays[i].length , 0 , result , index , arrays[i].length);
+        return null;
+    }
+}
