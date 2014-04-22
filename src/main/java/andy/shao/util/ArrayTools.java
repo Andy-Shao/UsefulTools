992851b4ebe003b8160c73431d8a6c81fe64b174
Andy-Shao <hj13035952619@126.com>
2014-04-23 00:00:15 +0800

    change the envoroment to 1.8

--- src/main/java/andy/shao/util/ArrayTools.java
+++ src/main/java/andy/shao/util/ArrayTools.java
@@ -1,6 +1,7 @@
 package andy.shao.util;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * 
@@ -8,15 +9,16 @@ import java.lang.reflect.Array;
  * Descript:<br>
  * Copyright: Copryright(c) Mar 4, 2014<br>
  * Encoding:UNIX UTF-8
+ * 
  * @author Andy.Shao
  *
  */
-public final class ArrayTools{
+public final class ArrayTools {
     private ArrayTools() {
         // TODO Auto-generated constructor stub
         throw new AssertionError("No support instance " + ArrayTools.class.getName());
     }
-    
+
     /**
      * 
      * @param array_type
@@ -24,11 +26,72 @@ public final class ArrayTools{
      * @return
      */
     @SuppressWarnings("unchecked")
-    public static <T> T[] mergeArrays(Class<T[]> array_type, T[]...arrays){
+    public static <T> T[] mergeArrays(Class<T[]> array_type , T[]... arrays) {
         int length = 0;
-        for(T[] array : arrays) length += array.length;
+        for (T[] array : arrays)
+            length += array.length;
         T[] result = (T[]) Array.newInstance(array_type.getComponentType() , length);
-        for(int i=0, index=0; i<arrays.length; index+=arrays[i].length, i++) System.arraycopy(arrays[i].length , 0 , result , index , arrays[i].length);
+        for (int i = 0 , index = 0 ; i < arrays.length ; index += arrays[i].length , i++)
+            System.arraycopy(arrays[i].length , 0 , result , index , arrays[i].length);
         return result;
+    }
+
+    /**
+     * 
+     * @param array
+     * @param index
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T[] removeItem(T[] array , int index) {
+        if (index >= array.length) return array;
+
+        return mergeArrays((Class<T[]>) array.getClass().getComponentType() , Arrays.copyOfRange(array , 0 , index) ,
+            Arrays.copyOfRange(array , index + 1 , array.length));
+    }
+
+    /**
+     * 
+     * @param array
+     * @param item
+     * @return
+     */
+    public static <T> T[] removeAllItem(T[] array , T item) {
+        for(int i=0; i<array.length; i++){
+            if(array[i].equals(item)) array = removeItem(array , i);
+        }
+        return array;
+    }
+
+    /**
+     * 
+     * @param array
+     * @param item
+     * @return
+     */
+    public static <T> T[] removeFirstItem(T[] array , T item) {
+        for(int i=0; i<array.length; i++){
+            if(array[i].equals(item)){
+                array = removeItem(array , i);
+                break;
+            }
+        }
+        return array;
+    }
+
+    /**
+     * 
+     * @param array
+     * @param item
+     * @return
+     */
+    public static <T> T[] removeLastItem(T[] array , T item) {
+        for(int i=array.length-1; i<0; i++){
+            if(array[i].equals(item)){
+                array = removeItem(array , i);
+                break;
+            }
+        }
+        return array;
     }
 }
