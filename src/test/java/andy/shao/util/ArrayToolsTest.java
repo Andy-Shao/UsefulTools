5a544a161962d165a4edd63f5db4d8511e3918f0
Andy-Shao <hj13035952619@126.com>
2014-05-08 11:56:22 +0800

    Update ArrayToolsTest.java

--- src/test/java/andy/shao/util/ArrayToolsTest.java
+++ src/test/java/andy/shao/util/ArrayToolsTest.java
@@ -4,10 +4,40 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author Andy
+ * 
+ */
 public class ArrayToolsTest {
 
-    @Test
-    public void mergeArrays(){
-        System.out.println(Arrays.toString(ArrayTools.mergeArray(Integer[].class , new Integer[]{1,2,3}, new Integer[]{4,5,6})));
-    }
+	@Test
+	public void removeItem() {
+		int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		Assert.assertArrayEquals(new int[] { 1, 2, 3, 5, 6, 7, 8, 9 }, ArrayTools.removeItem(array, 3));
+	}
+
+	@Test
+	public void mergeArray() {
+		int[] array = ArrayTools.mergeArray(int[].class, new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9 });
+		Assert.assertArrayEquals(array, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+	}
+	
+	@Test
+	public void removeAllItem(){
+		int[] array = new int[] { 1, 2, 3, 4, 3, 6, 7, 3, 9 };
+		Assert.assertArrayEquals(new int[] { 1, 2, 4, 6, 7, 9 }, ArrayTools.removeAllItem(array, 3));
+	}
+	
+	@Test
+	public void findFirstItem(){
+		int[] array = new int[] { 1, 2, 3, 4, 3, 6, 7, 3, 9 };
+		Assert.assertEquals(3, ArrayTools.findFirstItem(array, 4));
+	}
+	
+	@Test
+	public void findLastItem(){
+		int[] array = new int[] { 1, 2, 3, 4, 3, 6, 7, 3, 9 };
+		Assert.assertEquals(3, ArrayTools.findFirstItem(array, 4));
+	}
 }
