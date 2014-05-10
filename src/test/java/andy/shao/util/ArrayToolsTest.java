582388920d8e72401bdf35c539f6028ec104ea8b
Andy-Shao <hj13035952619@126.com>
2014-05-10 13:27:08 +0800

    add assert

--- src/test/java/andy/shao/util/ArrayToolsTest.java
+++ src/test/java/andy/shao/util/ArrayToolsTest.java
@@ -1,7 +1,6 @@
 package andy.shao.util;
 
-import java.util.Arrays;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -11,33 +10,51 @@ import org.junit.Test;
  */
 public class ArrayToolsTest {
 
-	@Test
-	public void removeItem() {
-		int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		Assert.assertArrayEquals(new int[] { 1, 2, 3, 5, 6, 7, 8, 9 }, ArrayTools.removeItem(array, 3));
-	}
+    @Test
+    public void findFirstItem() {
+        int[] array = new int[] {
+            1 , 2 , 3 , 4 , 3 , 6 , 7 , 3 , 9
+        };
+        Assert.assertEquals(3 , ArrayTools.findFirstItem(array , 4));
+    }
 
-	@Test
-	public void mergeArray() {
-		int[] array = ArrayTools.mergeArray(int[].class, new int[] { 1, 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9 });
-		Assert.assertArrayEquals(array, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-	}
-	
-	@Test
-	public void removeAllItem(){
-		int[] array = new int[] { 1, 2, 3, 4, 3, 6, 7, 3, 9 };
-		Assert.assertArrayEquals(new int[] { 1, 2, 4, 6, 7, 9 }, ArrayTools.removeAllItem(array, 3));
-	}
-	
-	@Test
-	public void findFirstItem(){
-		int[] array = new int[] { 1, 2, 3, 4, 3, 6, 7, 3, 9 };
-		Assert.assertEquals(3, ArrayTools.findFirstItem(array, 4));
-	}
-	
-	@Test
-	public void findLastItem(){
-		int[] array = new int[] { 1, 2, 3, 4, 3, 6, 7, 3, 9 };
-		Assert.assertEquals(3, ArrayTools.findFirstItem(array, 4));
-	}
+    @Test
+    public void findLastItem() {
+        int[] array = new int[] {
+            1 , 2 , 3 , 4 , 3 , 6 , 7 , 3 , 9
+        };
+        Assert.assertEquals(3 , ArrayTools.findFirstItem(array , 4));
+    }
+
+    @Test
+    public void mergeArray() {
+        int[] array = ArrayTools.mergeArray(int[].class , new int[] {
+            1 , 2 , 3 , 4
+        } , new int[] {
+            5 , 6 , 7 , 8 , 9
+        });
+        Assert.assertArrayEquals(array , new int[] {
+            1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9
+        });
+    }
+
+    @Test
+    public void removeAllItem() {
+        int[] array = new int[] {
+            1 , 2 , 3 , 4 , 3 , 6 , 7 , 3 , 9
+        };
+        Assert.assertArrayEquals(new int[] {
+            1 , 2 , 4 , 6 , 7 , 9
+        } , ArrayTools.removeAllItem(array , 3));
+    }
+
+    @Test
+    public void removeItem() {
+        int[] array = new int[] {
+            1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9
+        };
+        Assert.assertArrayEquals(new int[] {
+            1 , 2 , 3 , 5 , 6 , 7 , 8 , 9
+        } , ArrayTools.removeItem(array , 3));
+    }
 }
