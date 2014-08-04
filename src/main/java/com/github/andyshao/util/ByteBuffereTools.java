64f5d476fb1d21f1cb5b3616df1c37da35079d01
Andy-Shao <hj13035952619@126.com>
2014-08-04 15:02:18 +0800

    Add ByteBuffereTools

---
+++ src/main/java/com/github/andyshao/util/ByteBuffereTools.java
@@ -0,0 +1,26 @@
+package com.github.andyshao.util;
+
+import java.nio.ByteBuffer;
+
+/**
+ * 
+ * Descript:<br>
+ * Copyright: Copyright(c) Aug 4, 2014<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+public final class ByteBuffereTools {
+	private ByteBuffereTools() {
+		throw new AssertionError("No support instance " + ByteBuffereTools.class.getName());
+	}
+
+	/**
+	 * Only return a space which great than buffer.position() and less than buffer.limit()
+	 * @param buffer the buffer which has the info
+	 * @return return the byte array and the info in it
+	 */
+	public static byte[] usedArray(ByteBuffer buffer){
+		return ArrayTools.splitArray(buffer.array(), buffer.position(), buffer.limit());
+	}
+}
