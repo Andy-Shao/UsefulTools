9cb45af7d1504496bab7c6ba0896658348a01ec3
Andy-Shao <hj13035952619@126.com>
2014-09-23 17:29:02 +0800

    add convert test
    Signed-off-by: Andy-Shao <hj13035952619@126.com>

--- src/test/java/com/github/andyshao/convert/ConvertTest.java
+++ src/test/java/com/github/andyshao/convert/ConvertTest.java
@@ -1,8 +1,10 @@
 package com.github.andyshao.convert;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -41,5 +43,54 @@ public class ConvertTest {
 		String str = "[a, b, c, d, e, f, g]";
 		
 		Assert.assertEquals(str, Convert.OB_2_STR.convert(list));
+	}
+
+	@Test
+	public void testFloat(){
+		float f = 1.23F;
+		String fStr = "1.23";
+		
+		float myFloat = Convert.OB_2_FLOAT.convert(fStr);
+		Assert.assertThat(myFloat, Matchers.is(f));
+	}
+	
+	@Test
+	public void testInt(){
+		int i = 15_000;
+		String iStr = "15000";
+		
+		int myInt = Convert.OB_2_INT.convert(iStr);
+		Assert.assertThat(myInt, Matchers.is(i));
+	}
+	
+	@Test
+	public void testLong(){
+		long l = 15_000_000L;
+		String lStr = "15000000";
+		
+		long myLong = Convert.OB_2_LONG.convert(lStr);
+		Assert.assertThat(myLong, Matchers.is(l));
+	}
+	
+	@Test
+	public void testShort(){
+		short s = 150;
+		String sStr = "150";
+		
+		short myShort = Convert.OB_2_SHORT.convert(sStr);
+		Assert.assertThat(myShort, Matchers.is(s));
+	}
+	
+	@Test
+	public void testDouble(){
+		double d = 1.23456;
+		String dStr = "1.23456";
+		BigDecimal bigDecimal = new BigDecimal(dStr);
+		
+		double myDouble = Convert.OB_2_DOUBLE.convert(bigDecimal);
+		Assert.assertThat(myDouble, Matchers.is(d));
+		
+		myDouble = Convert.OB_2_DOUBLE.convert(dStr);
+		Assert.assertThat(myDouble, Matchers.is(d));
 	}
 }
