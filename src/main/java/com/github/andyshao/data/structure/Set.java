55060ae5e02ad109d21e1c04e519f419b65b82ac
Andy-Shao <hj13035952619@126.com>
2015-02-13 16:31:42 +0800

    update set

--- src/main/java/com/github/andyshao/data/structure/Set.java
+++ src/main/java/com/github/andyshao/data/structure/Set.java
@@ -18,28 +18,28 @@ public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
     public Object clone() throws CloneNotSupportedException;
 
     @SuppressWarnings("unchecked")
-    public default Set<D> set_difference(final Set<D> result , Set<D>... sets) throws CloneNotSupportedException {
+    public static <DATA> Set<DATA> set_difference(final Set<DATA> result , Set<DATA>... sets) throws CloneNotSupportedException {
         switch (sets.length) {
         case 0:
             break;
         case 1:
-            for (D data : sets[0])
+            for (DATA data : sets[0])
                 result.set_insert(data);
             break;
 
         default:
-            Set<D> temp = (Set<D>) result.clone();
-            for (Set<D> set : sets)
-                temp = this.set_difference((Set<D>) result.clone() , temp , set);
-            this.set_difference(result , temp);
+            Set<DATA> temp = (Set<DATA>) result.clone();
+            for (Set<DATA> set : sets)
+                temp = set_difference((Set<DATA>) result.clone() , temp , set);
+            set_difference(result , temp);
             break;
         }
         return result;
     }
 
-    public default Set<D> set_difference(final Set<D> result , Set<D> set1 , Set<D> set2) {
+    public static <DATA> Set<DATA> set_difference(final Set<DATA> result , Set<DATA> set1 , Set<DATA> set2) {
         //Insert the members from set1 not in set2.
-        for (D data : set1)
+        for (DATA data : set1)
             if (!set2.set_is_member(data)) result.set_insert(data);
 
         return result;
@@ -48,28 +48,28 @@ public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
     public abstract void set_insert(D data);
 
     @SuppressWarnings("unchecked")
-    public default Set<D> set_intersection(final Set<D> result , Set<D>... sets) throws CloneNotSupportedException {
+    public static <DATA> Set<DATA> set_intersection(final Set<DATA> result , Set<DATA>... sets) throws CloneNotSupportedException {
         switch (sets.length) {
         case 0:
             break;
         case 1:
-            for (D data : sets[0])
+            for (DATA data : sets[0])
                 result.set_insert(data);
             break;
 
         default:
-            Set<D> temp = (Set<D>) result.clone();
-            for (Set<D> set : sets)
-                temp = this.set_intersection((Set<D>) result.clone() , temp , set);
-            this.set_intersection(result , temp);
+            Set<DATA> temp = (Set<DATA>) result.clone();
+            for (Set<DATA> set : sets)
+                temp = set_intersection((Set<DATA>) result.clone() , temp , set);
+            set_intersection(result , temp);
             break;
         }
         return result;
     }
 
-    public default Set<D> set_intersection(final Set<D> result , Set<D> set1 , Set<D> set2) {
+    public static <DATA> Set<DATA> set_intersection(final Set<DATA> result , Set<DATA> set1 , Set<DATA> set2) {
         //Insert the member present in both sets.
-        for (D data : set1)
+        for (DATA data : set1)
             if (set2.set_is_member(data)) result.set_insert(data);
 
         return result;
@@ -82,33 +82,33 @@ public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
     public abstract void set_remove(D data);
 
     @SuppressWarnings("unchecked")
-    public default Set<D> set_union(Set<D> result , Set<D>... sets) throws CloneNotSupportedException {
+    public static <DATA> Set<DATA> set_union(Set<DATA> result , Set<DATA>... sets) throws CloneNotSupportedException {
         switch (sets.length) {
         case 0:
             break;
         case 1:
-            for (D data : sets[0])
+            for (DATA data : sets[0])
                 result.set_insert(data);
             break;
 
         default:
-            Set<D> temp = (Set<D>) result.clone();
-            for (Set<D> set : sets)
-                temp = this.set_union(result , temp , set);
-            this.set_union(result , temp);
+            Set<DATA> temp = (Set<DATA>) result.clone();
+            for (Set<DATA> set : sets)
+                temp = set_union(result , temp , set);
+            set_union(result , temp);
             break;
         }
 
         return result;
     }
 
-    public default Set<D> set_union(Set<D> result , Set<D> set1 , Set<D> set2) {
+    public static <DATA> Set<DATA> set_union(Set<DATA> result , Set<DATA> set1 , Set<DATA> set2) {
         //Insert the members of the first set.
-        for (D data : set1)
+        for (DATA data : set1)
             result.set_insert(data);
 
         //Insert the members of the second set.
-        for (D data : set2)
+        for (DATA data : set2)
             result.set_insert(data);
         
         return result;
