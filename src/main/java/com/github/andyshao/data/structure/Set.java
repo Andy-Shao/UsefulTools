fa82e3b3ff0cdb9c58f28e713584e7d78b69ba0f
Andy-Shao <hj13035952619@126.com>
2015-02-14 14:04:43 +0800

    revise set

--- src/main/java/com/github/andyshao/data/structure/Set.java
+++ src/main/java/com/github/andyshao/data/structure/Set.java
@@ -1,5 +1,8 @@
 package com.github.andyshao.data.structure;
 
+import java.util.HashSet;
+
+import com.github.andyshao.data.structure.convert.SetConvert;
 import com.github.andyshao.lang.Cleanable;
 
 /**
@@ -13,12 +16,10 @@ import com.github.andyshao.lang.Cleanable;
  *
  * @param <D> data
  */
-public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
-
-    public Object clone() throws CloneNotSupportedException;
+public interface Set<D> extends Cleanable , Iterable<D> {
 
     @SuppressWarnings("unchecked")
-    public static <DATA> Set<DATA> set_difference(final Set<DATA> result , Set<DATA>... sets) throws CloneNotSupportedException {
+    public static <DATA> Set<DATA> set_difference(final Set<DATA> result , Set<DATA>... sets) {
         switch (sets.length) {
         case 0:
             break;
@@ -28,9 +29,9 @@ public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
             break;
 
         default:
-            Set<DATA> temp = (Set<DATA>) result.clone();
+            Set<DATA> temp = new SetConvert<DATA>().convert(new HashSet<DATA>());
             for (Set<DATA> set : sets)
-                temp = set_difference((Set<DATA>) result.clone() , temp , set);
+                temp = set_difference(new SetConvert<DATA>().convert(new HashSet<DATA>()) , temp , set);
             set_difference(result , temp);
             break;
         }
@@ -48,7 +49,7 @@ public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
     public abstract void set_insert(D data);
 
     @SuppressWarnings("unchecked")
-    public static <DATA> Set<DATA> set_intersection(final Set<DATA> result , Set<DATA>... sets) throws CloneNotSupportedException {
+    public static <DATA> Set<DATA> set_intersection(final Set<DATA> result , Set<DATA>... sets) {
         switch (sets.length) {
         case 0:
             break;
@@ -58,9 +59,9 @@ public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
             break;
 
         default:
-            Set<DATA> temp = (Set<DATA>) result.clone();
+            Set<DATA> temp = new SetConvert<DATA>().convert(new HashSet<DATA>());
             for (Set<DATA> set : sets)
-                temp = set_intersection((Set<DATA>) result.clone() , temp , set);
+                temp = set_intersection(new SetConvert<DATA>().convert(new HashSet<DATA>()) , temp , set);
             set_intersection(result , temp);
             break;
         }
@@ -77,28 +78,15 @@ public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
 
     public abstract boolean set_is_member(D data);
 
-    public abstract boolean set_is_subset(LinkedSet<D> set1);
+    public abstract boolean set_is_subset(Set<D> set1);
 
     public abstract void set_remove(D data);
 
     @SuppressWarnings("unchecked")
-    public static <DATA> Set<DATA> set_union(Set<DATA> result , Set<DATA>... sets) throws CloneNotSupportedException {
-        switch (sets.length) {
-        case 0:
-            break;
-        case 1:
-            for (DATA data : sets[0])
-                result.set_insert(data);
-            break;
-
-        default:
-            Set<DATA> temp = (Set<DATA>) result.clone();
-            for (Set<DATA> set : sets)
-                temp = set_union(result , temp , set);
-            set_union(result , temp);
-            break;
-        }
-
+    public static <DATA> Set<DATA> set_union(Set<DATA> result , Set<DATA>... sets) {
+        Set<DATA> temp = new SetConvert<DATA>().convert(new HashSet<DATA>());
+        for (Set<DATA> set : sets)
+            set_union(result , temp , set);
         return result;
     }
 
@@ -110,7 +98,7 @@ public interface Set<D> extends Cleanable , Iterable<D> , Cloneable {
         //Insert the members of the second set.
         for (DATA data : set2)
             result.set_insert(data);
-        
+
         return result;
     }
 
--- src/main/java/com/github/andyshao/data/structure/convert/SetConvert.java
+++ src/main/java/com/github/andyshao/data/structure/convert/SetConvert.java
@@ -1,72 +1,62 @@
 package com.github.andyshao.data.structure.convert;
 
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
 import com.github.andyshao.convert.Convert;
-import com.github.andyshao.data.structure.LinkedSet;
 
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) Feb 14, 2015<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ * @param <D> data
+ */
 public class SetConvert<D> implements Convert<Set<D> , com.github.andyshao.data.structure.Set<D>> {
-
-    private static class MySet<D> implements com.github.andyshao.data.structure.Set<D> {
-        private final Set<D> set;
-
-        public MySet() {
-            this(new HashSet<D>());
-        }
-
-        public MySet(Set<D> set) {
-            this.set = set;
-        }
-
-        @Override
-        public void clean() {
-            this.set.clear();
-        }
-
-        @Override
-        public Object clone() throws CloneNotSupportedException {
-            return new MySet<D>();
-        }
-
-        @Override
-        public Iterator<D> iterator() {
-            return this.set.iterator();
-        }
-
-        @Override
-        public void set_insert(D data) {
-            this.set.add(data);
-        }
-
-        @Override
-        public boolean set_is_member(D data) {
-            return this.set.contains(data);
-        }
-
-        @Override
-        public boolean set_is_subset(LinkedSet<D> set1) {
-            for (D data : this.set)
-                if (!set1.set_is_member(data)) return false;
-
-            return true;
-        }
-
-        @Override
-        public void set_remove(D data) {
-            this.set.remove(data);
-        }
-
-        @Override
-        public int size() {
-            return this.set.size();
-        }
-
-    }
 
     @Override
     public com.github.andyshao.data.structure.Set<D> convert(final Set<D> in) {
-        return new MySet<D>(in);
+        return new com.github.andyshao.data.structure.Set<D>() {
+            @Override
+            public void clean() {
+                in.clear();
+            }
+
+            @Override
+            public Iterator<D> iterator() {
+                return in.iterator();
+            }
+
+            @Override
+            public void set_insert(D data) {
+                in.add(data);
+            }
+
+            @Override
+            public boolean set_is_member(D data) {
+                return in.contains(data);
+            }
+
+            @Override
+            public boolean set_is_subset(com.github.andyshao.data.structure.Set<D> set1) {
+                for (D data : in)
+                    if (!set1.set_is_member(data)) return false;
+
+                return true;
+            }
+
+            @Override
+            public void set_remove(D data) {
+                in.remove(data);
+            }
+
+            @Override
+            public int size() {
+                return in.size();
+            }
+        };
     }
 }
