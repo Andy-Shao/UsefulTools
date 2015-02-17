1018461dba7d7043181cd688ae1bba415702d39c
Andy-Shao <hj13035952619@126.com>
2015-02-17 16:32:58 +0800

    add AutoIncreaseArray

---
+++ src/main/java/com/github/andyshao/lang/AutoIncreaseArray.java
@@ -0,0 +1,136 @@
+package com.github.andyshao.lang;
+
+import java.lang.reflect.Array;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import com.github.andyshao.util.ArrayTools;
+
+public class AutoIncreaseArray<D> implements Iterable<D> , Cleanable {
+
+    private long actionAccount;
+    private D[] array;
+    private int arraySize;
+    private int end;
+    private int size;
+    private int start;
+
+    public AutoIncreaseArray() {
+        this(16);
+    }
+
+    public AutoIncreaseArray(int arraySize) {
+        this.arraySize = arraySize;
+        this.end = this.arraySize >> 1;
+        this.start = this.end;
+        this.actionAccount = 0;
+        this.size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void addHead(D data) {
+        if (data == null) throw new NullPointerException();
+        if (this.array == null) this.array = (D[]) Array.newInstance(data.getClass() , this.arraySize);
+
+        //Increase the array space
+        if (this.start == 0) {
+            int arraySize = this.newSize(this.array.length);
+            D[] temp = (D[]) Array.newInstance(data.getClass() , arraySize);
+            System.arraycopy(this.array , this.start , temp , arraySize >> 1 , this.size);
+            this.array = temp;
+            this.arraySize = arraySize;
+            this.start = arraySize >> 1;
+            this.end = this.start + this.size - 1;
+        }
+
+        if (this.size == 0) this.array[this.start] = data;
+        else this.array[--this.start] = data;
+        this.actionAccount++;
+        this.size++;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void addTail(D data) {
+        if (data == null) throw new NullPointerException();
+        if (this.array == null) this.array = (D[]) Array.newInstance(data.getClass() , this.arraySize);
+
+        //Increase the array space
+        if (this.end == this.arraySize - 1) {
+            int arraySize = this.newSize(this.array.length);
+            D[] temp = (D[]) Array.newInstance(data.getClass() , arraySize);
+            System.arraycopy(this.array , this.start , temp , arraySize >> 1 , this.size);
+            this.array = temp;
+            this.arraySize = arraySize;
+            this.start = arraySize >> 1;
+            this.end = this.start + this.size - 1;
+        }
+
+        if (this.size == 0) this.array[this.end] = data;
+        else this.array[++this.end] = data;
+        this.actionAccount++;
+        this.size++;
+    }
+
+    @Override
+    public void clean() {
+        this.actionAccount++;
+        this.end = this.arraySize >> 1;
+        this.start = this.end;
+        this.array = null;
+        this.size = 0;
+    };
+
+    public D get(int index) {
+        if (index < 0 || index >= this.size) throw new IndexOutOfBoundsException();
+        return this.array[index + this.start];
+    }
+
+    @Override
+    public Iterator<D> iterator() {
+        return new Iterator<D>() {
+            private final long actionAcount = AutoIncreaseArray.this.actionAccount;
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (this.actionAcount != AutoIncreaseArray.this.actionAccount) throw new ConcurrentModificationException();
+                return this.index < AutoIncreaseArray.this.size;
+            }
+
+            @Override
+            public D next() {
+                return AutoIncreaseArray.this.array[(this.index++) + AutoIncreaseArray.this.start];
+            }
+        };
+    }
+
+    public int newSize(int size) {
+        long result = ((long) size) << 1;
+        return result - (Integer.MAX_VALUE) > 0 ? Integer.MAX_VALUE : (int) result;
+    }
+
+    public D remove(int index) {
+        if (index < 0 || index >= this.size) throw new IndexOutOfBoundsException();
+        index = index + this.start;
+        D result = this.array[index];
+        this.array = ArrayTools.removeItem(this.array , index);
+        this.actionAccount++;
+        this.size--;
+        return result;
+    }
+
+    public D set(D data , int index) {
+        D result = null;
+        if (data == null) throw new NullPointerException();
+        if (index < 0 || index >= this.size) throw new IndexOutOfBoundsException();
+        index = index + this.start;
+        result = this.array[index];
+        this.array[index] = data;
+        this.actionAccount++;
+        return result;
+    }
+
+    public int size() {
+        return this.size;
+    }
+}
---
+++ src/test/java/com/github/andyshao/lang/AutoIncreaseArrayTest.java
@@ -0,0 +1,70 @@
+package com.github.andyshao.lang;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.andyshao.util.ArrayTools;
+
+public class AutoIncreaseArrayTest {
+
+    private volatile AutoIncreaseArray<Character> array;
+    private final Character[] data = ArrayTools.pack_unpack(new char[] {
+        'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' ,
+        's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'
+    } , Character[].class);
+
+    @Before
+    public void before(){
+        this.array = new AutoIncreaseArray<Character>();
+    }
+    
+    @Test
+    public void testInsertTail(){
+        Assert.assertThat(this.array.size() , Matchers.is(0));
+        
+        for(Character c : this.data){
+            this.array.addTail(c);
+        }
+        
+        Assert.assertThat(this.array.size() , Matchers.is(this.data.length));
+        Assert.assertThat(this.array.get(0) , Matchers.is(this.data[0]));
+        Assert.assertThat(this.array.get(this.data.length-1) , Matchers.is(this.data[this.data.length-1]));
+    }
+    
+    @Test
+    public void testInsertHead(){
+        Assert.assertThat(this.array.size() , Matchers.is(0));
+        
+        for(Character c : this.data){
+            this.array.addHead(c);
+        }
+        
+        Assert.assertThat(this.array.size() , Matchers.is(this.data.length));
+        Assert.assertThat(this.array.get(0) , Matchers.is(this.data[this.data.length-1]));
+        Assert.assertThat(this.array.get(this.data.length-1) , Matchers.is(this.data[0]));
+    }
+    
+    @Test
+    public void testRemove(){
+        testInsertTail();
+        
+        Character c = this.array.remove(this.data.length-1);
+        
+        Assert.assertThat(this.array.size() , Matchers.is(this.data.length - 1));
+        Assert.assertThat(c , Matchers.is('z'));
+    }
+    
+    @Test
+    public void testForeach(){
+        testInsertTail();
+        
+        String str = "";
+        for(Character c : this.array){
+            str += c;
+        }
+        
+        Assert.assertThat(str , Matchers.is("abcdefghijklmnopqrstuvwxyz"));
+    }
+}
